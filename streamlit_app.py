5e5fca82e7081a0b486328185c1c634af87171df
Kotaro Kitayama <whitebeardhisoka@gmail.com>
2024-11-19 14:54:53 +0900

    back to 3 threshold

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,8 @@ import streamlit as st
 import os
 from PIL import Image
 
-# カスタムCSSを追加して左右の余白を小さくする
-st.markdown(
-    """
-    <style>
-    .main .block-container {
-        padding-left: 1rem;
-        padding-right: 1rem;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # 閾値ごとのディレクトリ名
-thresholds = [50, 40, 30, 20, 10]
+thresholds = [50, 40, 30]
 directories = [os.path.join("data", f"NA-FA8H2_with_bboxes_threshold_{threshold}") for threshold in thresholds]
 
 # 各ディレクトリにある画像ファイルを取得
@@ -30,12 +17,10 @@ num_images = len(images_dict[directories[0]])
 
 # streamlit で画像を表示
 st.title("bounding box比較")
-st.header("左からVGTモデルの閾値50, 40, 30, 20, 10")
-desired_width = 150  # 画像の幅を設定（ピクセル単位）
+st.header("左からVGTモデルの閾値50, 40, 30")
 for i in range(num_images):
     cols = st.columns(len(directories))
     for col, directory in zip(cols, directories):
         image_path = images_dict[directory][i]
         with col:
-            # 画像の幅を設定
-            col.image(Image.open(image_path), caption=f"{directory} - page_{i + 1}.png", width=desired_width)
+            col.image(Image.open(image_path), caption=f"{directory} - page_{i + 1}.png", use_column_width=True)
